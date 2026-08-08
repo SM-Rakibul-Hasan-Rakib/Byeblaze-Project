0bc54819e9d2d490aee6388c51937e44a4121c90
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2026-08-08 23:11:58 +0600

    setup-done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Home></Home>
+      <Blogs></Blogs>
+    </>
+  );
+}
+
+export default App;
